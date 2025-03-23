f8759623a6d763a0fb225afafd6f49cdc30a4ddc
narayanan.skt <narayanan.skt@uni.club>
2025-03-23 17:16:47 +0530

    origin added

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://demo.lifehousechurch.in'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
